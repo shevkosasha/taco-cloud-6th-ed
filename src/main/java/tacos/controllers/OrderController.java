b9e6cd4169565efe023c412eb9ae210c2bebd56e
shevkosasha <shevkosasha@gmail.com>
2026-01-19 00:53:18 +0300

    add validation for taco

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -1,2 +1,26 @@
-package tacos.controllers;public class OrderController {
+package tacos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import tacos.models.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+
+//    @PostMapping
+//    public String processOrder(Order order) {
+//        log.info("Order submitted: " + order);
+//        return "redirect:/";
+//    }
 }
